44da5d84c6faefd3e303000a39245e696eddfd61
robin123-dev <robinrecruiter12355@gmail.com>
2026-07-22 21:46:28 +0530

    add /version endpoint

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ def health():
         "uptime": "24h" 
     }), 200
 
+@app.route('/version')
+def version():
+    return jsonify({
+        "version": "1.0.2",
+        "app": "Flask DevOps App",
+        "environment": os.getenv('ENV','development')
+    })
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
